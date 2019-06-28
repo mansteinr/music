8ac8de362c8dc0da9c3188b1849e800d4ec03357
manstein <manteinvip@sina.com>
2019-06-28 08:13:01 +0800

    scroll组件开发

---
+++ src/base/scroll.vue
@@ -0,0 +1,66 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+
+  export default {
+    // data() {
+      
+    // },
+    props: {
+      // 监听滚动事件类型，比如慢速滚动 快速滚动
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      data: {
+        type: Array,
+        default: () => []
+      }
+    },
+    methods: {
+      initScroll() {
+        if (!this.$refs.wrapper) return
+        this.scroll = new BScroll(this.$refs.wrapper, {
+          probeType: this.probeType,
+          click: this.click
+        })
+      },
+      enable() {
+        this.scroll && this.scroll.enable()
+      },
+      disable() {
+        this.scroll && this.scroll.disable()
+      },
+      refresh() {
+        this.scroll && this.scroll.refresh()
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+        this.initScroll()
+      })
+    },
+    watch: {
+      data() {
+        this.$nextTick(() => {
+          this.refresh()
+        })
+      }
+    }
+  }
+</script>
+
+
+<style lang="stylus" scoped>
+
+</style>
+
--- src/components/recommend/index.vue
+++ src/components/recommend/index.vue
@@ -1,37 +1,40 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <!-- 因为数据获取是异步过程 一开始就渲染数据为空 -->
-      <div v-if="recommends.length" class="slider-wrapper">
-        <slider>
-          <div v-for="(v, k) in recommends" :key="k">
-            <a :href="v.linkUrl">
-              <img :src="v.picUrl" alt="">
-            </a>
-          </div>
-        </slider>
-      </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌曲推荐</h1>
-        <ul>
-          <li v-for="(v, k) in discList" :key="k" class="item">
-            <div class="icon">
-              <img :src="v.imgurl" height="60" width="60" alt="">
+    <scroll class="recommend-content" :data="discList" ref="scroll">
+      <div>
+        <!-- 因为数据获取是异步过程 一开始就渲染数据为空 -->
+        <div v-if="recommends.length" class="slider-wrapper">
+          <slider>
+            <div v-for="(v, k) in recommends" :key="k">
+              <a :href="v.linkUrl">
+                <img :src="v.picUrl" alt="">
+              </a>
             </div>
-            <div class="text">
-              <h2 class="name" v-html="v.creator.name"></h2>
-              <p class="desc" v-html="v.dissname"></p>
-            </div>
-          </li>
-        </ul>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌曲推荐</h1>
+          <ul>
+            <li v-for="(v, k) in discList" :key="k" class="item">
+              <div class="icon">
+                <img @load="loadImage" :src="v.imgurl" height="60" width="60" alt="">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="v.creator.name"></h2>
+                <p class="desc" v-html="v.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script>
 import { getRecommend, getDiscList } from '@/api/recommend'
 import Slider from '@/base/slider'
+import Scroll from '@/base/scroll'
 
 export default {
   data () {
@@ -54,10 +57,21 @@ export default {
       getDiscList().then(res => {
         this.discList = res.data.list
       })
+    },
+    // 为了歌单能够下拉至底部，需要监听img，然后刷新scroll组件
+    //  this._getRecommend() this._getDiscList() 不知道谁先返回数据
+    // 这样的话better-scroll组件可以计算高度不够 少了上面轮播图img高度
+    loadImage() {
+      if (!this.checkLoaded) {
+        this.$refs.scroll.refresh()
+        this.checkLoaded = true
+      }
+      
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll
   }
 }
 </script>
